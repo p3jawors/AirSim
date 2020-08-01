7fd39e766edc7502fb452510cda1f4367f9be20b
Rajat Singhal <rajatsinghal564@gmail.com>
2020-08-01 13:27:14 +0530

    [Unity] Set Wind API implementation

--- Unity/AirLibWrapper/AirsimWrapper/Source/WorldSimApi.h
+++ Unity/AirLibWrapper/AirsimWrapper/Source/WorldSimApi.h
@@ -15,9 +15,9 @@ public:
 	virtual ~WorldSimApi();
 
     // ------ Level setting apis ----- //
-    virtual bool loadLevel(const std::string& level_name) { return false; };
-    virtual std::string spawnObject(std::string& object_name, const std::string& load_component, const Pose& pose, const Vector3r& scale, bool physics_enabled) { return ""; };
-    virtual bool destroyObject(const std::string& object_name) { return false; };
+    virtual bool loadLevel(const std::string& level_name) override { return false; };
+    virtual std::string spawnObject(std::string& object_name, const std::string& load_component, const Pose& pose, const Vector3r& scale, bool physics_enabled) override { return ""; };
+    virtual bool destroyObject(const std::string& object_name) override { return false; };
 
 	virtual bool isPaused() const override;
 	virtual void reset() override;
@@ -58,6 +58,8 @@ public:
     virtual void startRecording() override;
     virtual void stopRecording() override;
     virtual bool isRecording() const override;
+
+    virtual void setWind(const Vector3r& wind) const override;
 
 private:
 	SimModeBase * simmode_;
